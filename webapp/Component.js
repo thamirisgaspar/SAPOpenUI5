2c8b297ee51fa03038df42bf8930fde4c48d6c5c
T. Gaspar Da Silva <t.gaspar.da.silva@avanade.com>
2020-03-24 17:02:30 -0300

    Routing and Navigation

--- webapp/Component.js
+++ webapp/Component.js
@@ -22,7 +22,10 @@ sap.ui.define([
             this.setModel(oModel);
 
             // set dialog
-			this._helloDialog = new HelloDialog(this.getRootControl());
+            this._helloDialog = new HelloDialog(this.getRootControl());
+            
+            // create the views based on the url/hash
+			this.getRouter().initialize();
         },
         exit : function() {
             this._helloDialog.destroy();
