3882f365cd8cd50ea2ee5086861bfe61aaaebd15
T. Gaspar Da Silva <t.gaspar.da.silva@avanade.com>
2020-03-24 15:24:49 -0300

    Reuse Dialogs

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,8 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+	"./controller/HelloDialog"
+], function (UIComponent, JSONModel, HelloDialog) {
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata : {
@@ -21,12 +21,15 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             this.setModel(oModel);
 
-            // set i18n model
-            var i18nModel = new ResourceModel({
-                bundleName : "sap.ui.demo.walkthrough.i18n.i18n"
-            });
-            
-            this.setModel(i18nModel, "i18n");
-        }
+            // set dialog
+			this._helloDialog = new HelloDialog(this.getRootControl());
+        },
+        exit : function() {
+            this._helloDialog.destroy();
+            delete this._helloDialog;
+        },
+        openHelloDialog : function () {
+		    this._helloDialog.open();
+		}
     });
 });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,7 +2,10 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast"
 ], function (Controller, MessageToast) {
-   "use strict";
-   return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-   });
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        onOpenDialog : function () {
+            this.getOwnerComponent().openHelloDialog();
+        }
+    });
 });
