dd4be54f15c2f8ae7dd3dd75b0ff5716630901c5
T. Gaspar Da Silva <t.gaspar.da.silva@avanade.com>
2020-03-24 13:07:36 -0300

    Controls

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-sap.ui.define([], function () {
+sap.ui.define([
+    "sap/m/Text"
+], function (Text) {
     "use strict";
-    alert("UI5 is ready");
+    new Text({
+		text: "Hello World"
+	}).placeAt("content");
 });
